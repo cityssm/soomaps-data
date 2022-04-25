3a8030e66189d38ef2b89eec66ef6491ae243ede
dangowans <d.gowans@cityssm.on.ca>
2022-04-25 09:24:24 -0400

    more layers

--- layers.js
+++ layers.js
@@ -5,6 +5,21 @@ export const layers = [
         fields: ["CIVICNUMBER", "STREETNAME", "POSTALCODE", "MUNICIPALITY", "LATITUDE", "LONGITUDE"]
     },
     {
+        layerName: "busStops",
+        layerURL: "https://enterprise.ssmic.com/server/rest/services/SooMaps/SooMaps_GeneralLayers/MapServer/3",
+        fields: ["LABEL", "STOPNUMBER", "SHELTERTYPE", "SERVICEDAYS", "TRANSFERPOINT", "BENCH", "GARBAGECAN", "SHELTER", "LATITUDE", "LONGITUDE"]
+    },
+    {
+        layerName: "heritageSites",
+        layerURL: "https://enterprise.ssmic.com/server/rest/services/SooMaps/SooMaps_GeneralLayers/MapServer/9",
+        fields: ["COMMONNAME", "DESCRIPTIONOFPROPERTY", "TYPEHERITAGEFEATURE", "HERITAGECLASSIFICATION", "HERITAGEDISTRICT", "YROFCONSTRUCTION", "DESIGNATING_ACT"]
+    },
+    {
+        layerName: "parcels",
+        layerURL: "https://enterprise.ssmic.com/server/rest/services/SooMaps/SooMaps_GeneralLayers/MapServer/19",
+        fields: ["ROLLNUMBER", "MPACCIVIC", "MPACSTREETNAME", "SHAPE_Length", "SHAPE_Area"]
+    },
+    {
         layerName: "streets",
         layerURL: "https://enterprise.ssmic.com/server/rest/services/SooMaps/SooMaps_GeneralLayers/MapServer/1",
         fields: ["FULLNAME", "STREETPREFIX", "STREETNAME", "STREETTYPE", "STREETSUFFIX", "LEFTFROMADDRESS", "LEFTTOADDRESS", "RIGHTFROMADDRESS", "RIGHTTOADDRESS"]
